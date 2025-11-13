024f17156bfb2985a86b96be4d12956f372dc400
NhenryMg <mongegonzalezhenry@gmail.com>
2025-11-13 18:34:49 +0000

    Inicializando el ionic desde cero y ya esta instalado correctamente

---
+++ variedades-dona-luz/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'variedades-dona-luz',
+  webDir: 'dist'
+};
+
+export default config;
